694b296e633f04140a20962c56e249e858629885
Dan Stocker <dan@kwaia.com>
2013-06-02 21:01:29 +0200

    grunt based testing & build

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+/*jshint node:true */
+module.exports = function (grunt) {
+    "use strict";
+
+    var params = {
+        files: [
+            'js/namespace.js',
+            'js/EventPath.js',
+            'js/Event.js',
+            'js/EventSpace.js',
+            'js/exports.js'
+        ],
+
+        test: [
+            'js/jsTestDriver.conf'
+        ],
+
+        globals: {
+            dessert: true,
+            troop  : true,
+            sntls  : true
+        }
+    };
+
+    // invoking common grunt process
+    require('common-gruntfile')(grunt, params);
+};
