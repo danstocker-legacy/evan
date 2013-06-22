a7478246457d428e90b5d87f5ba85710739aa44e
Dan Stocker <dan@kwaia.com>
2013-06-22 19:10:22 +0200

    common gruntfile pudated to 0.1.2 & copyright info moved inside package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,6 @@ module.exports = function (grunt) {
 
     var params = {
         files: [
-            'js/license.js',
             'js/namespace.js',
             'js/PathCollection.js',
             'js/Event.js',
