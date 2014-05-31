171c7e8801ad8275daf2bdf156cdf4aaa173f0ef
Dan Stocker <dan@kwaia.com>
2014-05-31 08:51:31 +0100

    adds library alias

--- js/namespace.js
+++ js/namespace.js
@@ -4,6 +4,7 @@
 /*global require */
 /** @namespace */
 var evan = {},
+    e$ = evan,
     dessert = dessert || require('dessert'),
     troop = troop || require('troop'),
     sntls = sntls || require('sntls');
